23196cd31f415b6197a338b781519c24d8a2fa51
Oliver Murray <20854536+om154@users.noreply.github.com>
2021-09-03 15:11:29 +1000

    use light theme logo for Shippit

--- src/components/Pill/ShippitPill/index.tsx
+++ src/components/Pill/ShippitPill/index.tsx
@@ -10,12 +10,12 @@ interface ShippitPillProps {
 }
 
 const ShippitPill = ({ className }: ShippitPillProps) => (
-  <Pill className={cn('bg-[#0074DC]', className)}>
+  <Pill className={cn('bg-white', className)}>
     <ShippitLogo className="w-16 h-5 mr-2" />
-    <a className="flex ml-2 mr-1 text-white" href="https://www.shippit.com/" target="_blank" rel="noreferrer">
+    <a className="flex ml-2 mr-1 text-neutral-800" href="https://www.shippit.com/" target="_blank" rel="noreferrer">
       <ExternalLinkIcon className="w-5 h-5 fill-current" />
     </a>
-    <a className="flex ml-2 mr-1 text-white" href="https://www.linkedin.com/company/shippit" target="_blank" rel="noreferrer">
+    <a className="flex ml-2 mr-1 text-[#0a66c2]" href="https://www.linkedin.com/company/shippit" target="_blank" rel="noreferrer">
       <LinkedInLogo className="w-5 h-5 fill-current" />
     </a>
     <a className="flex ml-2" href="https://www.crunchbase.com/organization/shippt" target="_blank" rel="noreferrer">

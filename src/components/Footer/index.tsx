db383306d197701b624e3b9b18708a87305d4e03
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-05-02 12:04:31 +0200

    refine styles and add transitions

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,20 +3,20 @@ import { ReactComponent as GitHubLogo } from '../../assets/github-logo.svg';
 import { ReactComponent as TwitterLogo } from '../../assets/twitter-logo.svg';
 
 const Footer = () => (
-  <div className="box-border flex justify-center w-full px-4 mx-auto text-center">
+  <div className="box-border flex justify-center w-full px-2 mx-auto text-center rounded-lg bg-white/70">
     {process.env.REACT_APP_TWITTER_URL && (
     <a className="flex m-2" href={process.env.REACT_APP_TWITTER_URL} target="_blank" rel="noreferrer">
-        <TwitterLogo className="w-5 h-5 fill-neutral-500 hover:fill-neutral-600" />
+        <TwitterLogo className="w-5 h-5 transition-all duration-250 fill-neutral-500 hover:fill-neutral-700 " />
     </a>
     )}
     {process.env.REACT_APP_GITHUB_URL && (
     <a className="flex m-2" href={process.env.REACT_APP_GITHUB_URL} target="_blank" rel="noreferrer">
-        <GitHubLogo className="w-5 h-5 fill-neutral-500 hover:fill-neutral-600" />
+        <GitHubLogo className="w-5 h-5 transition-all duration-250 fill-neutral-500 hover:fill-neutral-700 " />
     </a>
     )}
     {process.env.REACT_APP_LINKEDIN_URL && (
     <a className="flex m-2" href={process.env.REACT_APP_LINKEDIN_URL} target="_blank" rel="noreferrer">
-        <LinkedInLogo className="w-5 h-5 fill-neutral-500 hover:fill-neutral-600" />
+        <LinkedInLogo className="w-5 h-5 transition-all duration-250 fill-neutral-500 hover:fill-neutral-700 " />
     </a>
     )}
   </div>
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -9,11 +9,11 @@ interface ListItemProps {
 }
 
 const LinkListItem = ({ title, path, active }: ListItemProps) => (
-  <li>
+  <li className='contents'>
     <Link to={path} className={classNames(
-      'px-1 md:px-2 py-1 font-sans text-sm hover:bg-neutral-200  hover:text-primary-700 rounded-lg leading-5',
+      'px-1 md:px-2 py-1 font-sans text-sm hover:bg-neutral-200  hover:text-primary-900 rounded-lg leading-5 transiiton-all duration-200',
       {
-        'text-primary-700': active,
+        'text-primary-900': active,
         'text-neutral-600':!active
       }
     )}>{title}</Link>
@@ -25,7 +25,7 @@ const Navigation = () => {
 
   return (
     <div className="sticky top-0 z-10 flex items-center justify-center py-4 md:px-1 isolate md:justify-between">
-      <nav className='relative flex p-1 border rounded-lg shadow-md border-neutral-200 bg-white/70 backdrop-blur-md'>
+      <nav className='relative flex p-1 border rounded-lg shadow-md md:px-2 border-neutral-200 bg-white/70 backdrop-blur-md'>
         <ul className='flex flex-row gap-1 list-none md:gap-0'>
           <LinkListItem title="Home" path="/" active={location.pathname === '/'} />
           <LinkListItem title="About" path="/about" active={location.pathname === '/about'} />

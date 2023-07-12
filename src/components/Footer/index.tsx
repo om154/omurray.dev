76eca58188b0f012988ba1380f86c1db11ab053b
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-12 18:02:15 +0200

    fix up GitHub and Twitter icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,6 +3,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import LinkedInLogo from '../shared/LinkedInLogo';
+import GitHubLogo from './GitHubLogo';
+import TwitterLogo from './TwitterLogo';
 
 const Footer = () => (
   <footer className="box-border flex justify-center w-full pl-2 mx-auto text-center rounded-lg bg-white/70">
@@ -13,12 +15,12 @@ const Footer = () => (
     )}
     {process.env.REACT_APP_GITHUB_URL && (
       <Link className="flex m-2" href={process.env.REACT_APP_GITHUB_URL} target="_blank" rel="noreferrer">
-        <Image src='/static/svg/github-logo.svg' width={20} height={20} alt='github' className="transition-all duration-250 fill-neutral-500 hover:fill-neutral-700 " />
+        <GitHubLogo className="transition-all duration-250 fill-neutral-500 hover:fill-neutral-700" />
       </Link>
     )}
     {process.env.REACT_APP_LINKEDIN_URL && (
       <Link className="flex m-2" href={process.env.REACT_APP_LINKEDIN_URL} target="_blank" rel="noreferrer">
-        <Image src='/static/svg/twitter-logo.svg' width={20} height={20} alt='twitter' className="transition-all duration-250 fill-neutral-500 hover:fill-neutral-700 " />
+        <TwitterLogo className="transition-all duration-250 fill-neutral-500 hover:fill-neutral-700" />
       </Link>
     )}
   </footer>

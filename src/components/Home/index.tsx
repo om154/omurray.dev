89d03b43bbfd13c1ff6dd6ea2890009a62df084f
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-13 22:04:28 +0200

    add margin to greeting

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,7 +3,7 @@ import Greeting from '../Greeting';
 const Home = () => {
   return (
     <div className='grid grid-cols-1 gap-4 mt-2 md:mt-0 sm:grid-flow-row-dense sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-      <div className='h-full row-span-2 sm:col-span-2 lg:aspect-square'>
+      <div className='h-full row-span-2 mb-4 lg:mb-0 sm:col-span-2 lg:aspect-square'>
         <Greeting />
       </div>
       <div className='aspect-[2] sm:col-span-2 px-1 pb-2 rounded-lg bg-neutral-800'>

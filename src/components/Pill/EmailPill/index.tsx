cef8c689fabf9f9dd80ff061cda1ed46efbe0e62
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-13 16:24:11 +0200

    fix bug: email environment variable

--- src/components/Pill/EmailPill/index.tsx
+++ src/components/Pill/EmailPill/index.tsx
@@ -6,16 +6,16 @@ interface EmailPillProps {
 }
 
 const EmailPill = ({ className }: EmailPillProps) => {
-  if (process.env.NEXT_PUBLIC__USER_EMAIL_ADDRESS === undefined) return null;
+  if (process.env.NEXT_PUBLIC_USER_EMAIL_ADDRESS === undefined) return null;
 
   return (
-    <Link href={`mailto:${process.env.NEXT_PUBLIC__USER_EMAIL_ADDRESS}`} className={cn('inline-block px-3 py-2 font-bold bg-white rounded-lg shadow-md text-primary-900', className)}>
+    <Link href={`mailto:${process.env.NEXT_PUBLIC_USER_EMAIL_ADDRESS}`} className={cn('inline-block px-3 py-2 font-bold bg-white rounded-lg shadow-md text-primary-900', className)}>
       <div className="flex items-center my-1">
         <span className="mr-2 font-serif leading-tight no-underline select-none font-regular text-md md:text-lg">
           📫
         </span>
         <span className="mr-1 font-serif leading-tight no-underline select-none font-regular text-md md:text-lg">
-          { `${process.env.NEXT_PUBLIC__USER_EMAIL_ADDRESS}`}
+          { `${process.env.NEXT_PUBLIC_USER_EMAIL_ADDRESS}`}
         </span>
       </div>
     </Link>

9a23ea01e9c59765a125c62b48f196a651021f32
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-13 22:07:19 +0200

    remove shadow

--- src/components/Hero/Interests/index.tsx
+++ src/components/Hero/Interests/index.tsx
@@ -3,7 +3,7 @@ import { ChildrenProp } from '../../../utils/props';
 import { HeroProps } from '../props';
 
 const InterestsHero = ({ imgPath, children }: HeroProps & ChildrenProp) => (
-  <div className="flex flex-col px-6 py-4 overflow-hidden rounded-lg shadow-lg md:flex-row-reverse md:gap-8 xl:col-span-2 bg-neutral-100 lg:justify-between md:px-8 md:py-6 xl:max-w-5xl xl:m-auto xl:h-full">
+  <div className="flex flex-col px-6 py-4 overflow-hidden rounded-lg md:flex-row-reverse md:gap-8 xl:col-span-2 bg-neutral-100 lg:justify-between md:px-8 md:py-6 xl:max-w-5xl xl:m-auto xl:h-full">
     <div className="flex flex-col justify-center flex-1 order-last mt-4 lg:m-0">
       <div>
         {children}

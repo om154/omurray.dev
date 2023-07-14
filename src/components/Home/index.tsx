254ee806c911992710cff7da03dbde9c3aca26b6
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-14 19:24:41 +0200

    add one more hero for Bend

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,7 +4,7 @@ import PhotoHero from '../PhotoHero';
 const Home = () => {
   return (
     <div className='grid grid-cols-1 gap-4 mt-2 overflow-hidden md:mt-0 sm:grid-flow-row-dense sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-      <div className='h-full row-span-2 mb-4 lg:mb-2 sm:col-span-2 '>
+      <div className='h-full row-span-2 mb-4 sm:col-span-2 '>
         <Greeting />
       </div>
       <div className='row-span-2 lg:mb-0 sm:col-span-2'>
@@ -12,6 +12,9 @@ const Home = () => {
       </div>
       <div className='row-span-2 lg:mb-0 sm:col-span-2'>
         <PhotoHero title='Norway, 2023' emoji='🇳🇴' directory='norway' />
+      </div>
+      <div className='row-span-2 lg:mb-0 sm:col-span-2'>
+        <PhotoHero title='Oregon, 2022' emoji='⛰️' directory='bend' />
       </div>
     </div>
   );

a1d5c652a5968ec0609863db7c618510b4075f92
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-14 19:19:16 +0200

    refactor Japan hero to be generic, add a Norway one

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,18 @@
 import Greeting from '../Greeting';
-import JapanHero from '../JapanHero';
+import PhotoHero from '../PhotoHero';
 
 const Home = () => {
   return (
     <div className='grid grid-cols-1 gap-4 mt-2 overflow-hidden md:mt-0 sm:grid-flow-row-dense sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-      <div className='h-full row-span-2 mb-4 lg:mb-0 sm:col-span-2 lg:aspect-square'>
+      <div className='h-full row-span-2 mb-4 lg:mb-2 sm:col-span-2 '>
         <Greeting />
       </div>
-      <JapanHero />
+      <div className='row-span-2 lg:mb-0 sm:col-span-2'>
+        <PhotoHero title='Japan, 2023' emoji='🇯🇵' directory='japan' />
+      </div>
+      <div className='row-span-2 lg:mb-0 sm:col-span-2'>
+        <PhotoHero title='Norway, 2023' emoji='🇳🇴' directory='norway' />
+      </div>
     </div>
   );
 };

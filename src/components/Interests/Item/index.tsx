31e6044f94500cb9ff59eca2735d486da5cb97cd
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-14 14:11:31 +0200

    update font weight

--- src/components/Interests/Item/index.tsx
+++ src/components/Interests/Item/index.tsx
@@ -15,7 +15,7 @@ interface ItemProps {
 const Item = ({ item: { title, paragraphs, imagePath } }: ItemProps) => (
   <InterestsHero imgPath={imagePath}>
     <div>
-      <h2 className="box-border font-serif text-xl font-bold leading-tight tracking-tight text-center md:text-3xl lg:text-4xl text-primary-800 md:text-left">
+      <h2 className="box-border font-serif text-xl leading-tight tracking-tight text-center font-regular md:text-3xl lg:text-4xl text-primary-800 md:text-left">
         {title}
       </h2>
       <div>

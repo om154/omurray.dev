80b271fa1fdc598f0f0cce7dac7e0e197b196b9a
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-14 22:12:53 +0200

    update hover interactions

--- src/components/PhotoHero/index.tsx
+++ src/components/PhotoHero/index.tsx
@@ -16,7 +16,7 @@ const PhotoHero = ({ data }: Props) => {
   const { title, month, year, emoji, directory } = data;
 
   return (
-    <div className='aspect-[2] sm:col-span-2 p-4 rounded-lg bg-neutral-100 hover:bg-neutral-300 transition-colors duration-200 ease-in-out relative flex flex-col justify-center items-center'>
+    <div className='aspect-[2] sm:col-span-2 p-4 rounded-lg bg-neutral-100 relative flex flex-col justify-center items-center'>
       <div className='w-full mb-2 text-lg leading-none font-regular text-primary-900 top-4 left-4'>
         <div className='flex flex-row items-center'>
           {emoji && <span className='mr-2 text-2xl'>{emoji}</span>}
@@ -27,14 +27,14 @@ const PhotoHero = ({ data }: Props) => {
             </div> : <div />}
         </div>
       </div>
-      <div className='flex flex-col w-full gap-2 my-4'>
+      <div className='flex flex-col w-full gap-2 my-4 transition-transform duration-300 ease-out hover:scale-[1.02]'>
         <div className="relative flex flex-row justify-center w-full gap-2">
           <Image src={`/static/img/${directory}/1.jpg`} alt='japan' height={64} width={208} className='hidden md:block object-cover object-center rounded-xl h-[104px]'/>
           <Image src={`/static/img/${directory}/2.jpg`} alt='japan' height={128} width={208} className='object-cover object-center rounded-xl h-[158px] md:h-[208px]'/>
           <Image src={`/static/img/${directory}/3.jpg`} alt='japan' height={85} width={85} className='hidden md:block object-cover object-center rounded-[100px] h-[160px] self-end'/>
         </div>
         <div className="relative flex flex-row justify-center w-full gap-2">
-          <Image src={`/static/img/${directory}/4.jpg`} alt='japan' height={128} width={208} className='object-cover object-center border-8 border-solid border-neutral-300 rounded-xl mt-[-30px] ml-[-8px] md:mt-[-112px] md:mb-[-8px] md:mr-[-8px] md:ml-[-80px]' />
+          <Image src={`/static/img/${directory}/4.jpg`} alt='japan' height={128} width={208} className='object-cover object-center border-8 border-solid border-neutral-100 rounded-xl mt-[-30px] ml-[-8px] md:mt-[-112px] md:mb-[-8px] md:mr-[-8px] md:ml-[-80px]' />
           <Image src={`/static/img/${directory}/5.jpg`} alt='japan' height={150} width={95} className='object-cover object-center rounded-xl h-[150px] w-[95px] rounded-tl-sm' />
         </div>
       </div>

957b60fc014112bc38dc472d5544c9cfdc774605
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-14 18:53:31 +0200

    hide overflowing content

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,7 +3,7 @@ import JapanHero from '../JapanHero';
 
 const Home = () => {
   return (
-    <div className='grid grid-cols-1 gap-4 mt-2 md:mt-0 sm:grid-flow-row-dense sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+    <div className='grid grid-cols-1 gap-4 mt-2 overflow-hidden md:mt-0 sm:grid-flow-row-dense sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       <div className='h-full row-span-2 mb-4 lg:mb-0 sm:col-span-2 lg:aspect-square'>
         <Greeting />
       </div>

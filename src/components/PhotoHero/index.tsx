0a3280a7bb17f1b0560a5f743f6759d8736b9009
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-14 22:16:46 +0200

    modify gap

--- src/components/PhotoHero/index.tsx
+++ src/components/PhotoHero/index.tsx
@@ -33,7 +33,7 @@ const PhotoHero = ({ data }: Props) => {
           <Image src={`/static/img/${directory}/2.jpg`} alt='japan' height={128} width={208} className='object-cover object-center rounded-xl h-[158px] md:h-[208px]'/>
           <Image src={`/static/img/${directory}/3.jpg`} alt='japan' height={85} width={85} className='hidden md:block object-cover object-center rounded-[100px] h-[160px] self-end'/>
         </div>
-        <div className="relative flex flex-row justify-center w-full gap-2">
+        <div className="relative flex flex-row justify-center w-full gap-x-0 gap-y-2 md:gap-2">
           <Image src={`/static/img/${directory}/4.jpg`} alt='japan' height={128} width={208} className='object-cover object-center border-8 border-solid border-neutral-100 rounded-xl mt-[-30px] ml-[-8px] md:mt-[-112px] md:mb-[-8px] md:mr-[-8px] md:ml-[-80px]' />
           <Image src={`/static/img/${directory}/5.jpg`} alt='japan' height={150} width={95} className='object-cover object-center rounded-xl h-[150px] w-[95px] rounded-tl-sm' />
         </div>

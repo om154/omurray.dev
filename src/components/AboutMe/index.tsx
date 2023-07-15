2579876ae12c942231ae3fa3b3d1ed09e22c124c
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-15 15:54:51 +0200

    code cleanup: create a card component for about me cards

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -11,6 +11,7 @@ import TwinePill from '../Pill/TwinePill';
 import ContactLinks from '../Navigation/ContactLinks';
 import aboutMeImg from 'public/static/img/oliver.jpeg'
 import AnimateDown from '../animations/AnimateDown';
+import { ReactNode } from 'react';
 
 export function calculateAge(birthday: string) {
   const splitDate = birthday.split('-');
@@ -26,6 +27,74 @@ export function calculateAge(birthday: string) {
   const age = Math.abs(year - 1970);
   return age;
 }
+
+interface AboutMeCardProps {
+  index: number
+  content: ReactNode
+  Pill: ReactNode
+}
+
+const AboutMeCard = ({ index, content, Pill }: AboutMeCardProps) => (
+  <AnimateDown delay={.1 + (index * .1)} className='flex flex-col justify-between p-5 font-sans transition-colors duration-200 ease-in-out rounded-lg aspect-squar bg-neutral-100 hover:bg-neutral-200'>
+    <P>
+      {content}
+    </P>
+    <div className="flex flex-col items-center mt-3 space-x-2 lg:space-x-0 lg:flex-row lg:justify-around">
+      {Pill}
+    </div>
+  </AnimateDown>
+)
+
+interface AboutMeCard {
+  content: ReactNode
+  Pill: ReactNode
+}
+
+const cards: AboutMeCard[] = [
+  // twine
+  {
+    content: <>
+      At twine we&apos;re focused on helping connect remote teams, and are fully remote ourselves!
+      {' '}
+      Our platform makes remote work fun & engaging by connecting employees at key moments such as onboarding, all hands and other events.
+    </>,
+    Pill: <TwinePill className="my-1 select-none lg:m-0" />
+  },
+  // Glimpse
+  {
+    content: <>
+      I started out working on the problem of connection for remote teams at
+      {' '}
+      <Link className="inline-block decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.joinglimpse.com/" target="_blank" rel="noreferrer">Glimpse</Link>
+      , but we teamed up when twine acquired us in early 2022.
+    </>,
+    Pill: <GlimpsePill className="mt-1 select-none lg:m-0" />
+  },
+  // Shippit
+  {
+    content: <>
+      In the past I worked in Engineering at
+      {' '}
+      <Link className="decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.shippit.com/" target="_blank" rel="noreferrer">Shippit</Link>
+      , a Sydney-based start-up
+      creating the network that brings
+      people & goods together in the leanest way possible.
+    </>,
+    Pill: <ShippitPill className="my-1 select-none lg:m-0" />
+  },
+  // Accenture
+  {
+    content: <>
+      I also spent time at
+      {' '}
+      <Link className="decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.accenture.com/" target="_blank" rel="noreferrer">Accenture</Link>
+      {' '}
+      developing large-scale web
+      applications to help their clients effectively capture, manage and analyze their data.
+    </>,
+    Pill: <AccenturePill className="my-1 select-none lg:my-0 lg:ml-1" />
+  }
+]
 
 const AboutMe = () => (
   <Section alignTop>
@@ -50,53 +119,9 @@ const AboutMe = () => (
           <H3>Experience building software</H3>
         </div>
         <div className='grid grid-cols-1 gap-4 mt-4 sm:grid-flow-row-dense lg:grid-cols-2'>
-          <AnimateDown delay={.1 + 1 * .1} className='flex flex-col justify-between p-5 font-sans transition-colors duration-200 ease-in-out rounded-lg aspect-squar bg-neutral-100 hover:bg-neutral-200'>
-            <P>
-              At twine we&apos;re focused on helping connect remote teams, and are fully remote ourselves!
-              {' '}
-              Our platform makes remote work fun & engaging by connecting employees at key moments such as onboarding, all hands and other events.
-              <div className="flex flex-col items-center mt-3 space-x-2 lg:space-x-0 lg:flex-row lg:justify-around">
-                <TwinePill className="my-1 select-none lg:m-0" />
-              </div>
-            </P>
-          </AnimateDown>
-          <AnimateDown delay={.1 + 2 * .1} className='flex flex-col justify-between p-5 font-sans transition-colors duration-200 ease-in-out rounded-lg aspect-squar bg-neutral-100 hover:bg-neutral-200'>
-            <P>
-              I started out working on the problem of connection for remote teams at
-              {' '}
-              <Link className="inline-block decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.joinglimpse.com/" target="_blank" rel="noreferrer">Glimpse</Link>
-              , but we teamed up when twine acquired us in early 2022.
-            </P>
-            <div className="flex flex-col items-center mt-3 space-x-2 lg:space-x-0 lg:flex-row lg:justify-around">
-              <GlimpsePill className="mt-1 select-none lg:m-0" />
-            </div>
-          </AnimateDown>
-          <AnimateDown delay={.1 + 3 * .1} className='flex flex-col justify-between p-5 font-sans transition-colors duration-200 ease-in-out rounded-lg aspect-squar bg-neutral-100 hover:bg-neutral-200'>
-            <P>
-              In the past I worked in Engineering at
-              {' '}
-              <Link className="decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.shippit.com/" target="_blank" rel="noreferrer">Shippit</Link>
-              , a Sydney-based start-up
-              creating the network that brings
-              people & goods together in the leanest way possible.
-            </P>
-            <div className="flex flex-col items-center mt-3 space-y-2 md:space-x-2 lg:space-y-0 lg:space-x-0 lg:flex-row lg:justify-around">
-              <ShippitPill className="my-1 select-none lg:m-0" />
-            </div>
-          </AnimateDown>
-          <AnimateDown delay={.1 + 4 * .1} className='flex flex-col justify-between p-5 font-sans transition-colors duration-200 ease-in-out rounded-lg aspect-squar bg-neutral-100 hover:bg-neutral-200'>
-            <P>
-              I also spent time at
-              {' '}
-              <Link className="decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.accenture.com/" target="_blank" rel="noreferrer">Accenture</Link>
-              {' '}
-              developing large-scale web
-              applications to help their clients effectively capture, manage and analyze their data.
-            </P>
-            <div className="flex flex-col items-center mt-3 space-y-2 md:space-x-2 lg:space-y-0 lg:space-x-0 lg:flex-row lg:justify-around">
-              <AccenturePill className="my-1 select-none lg:my-0 lg:ml-1" />
-            </div>
-          </AnimateDown>
+          {cards.map((c, i) => (
+            <AboutMeCard index={i} content={c.content} Pill={c.Pill} />
+          ))}
         </div>
       </AboutMeHero>
     </div>

9126914e91f2d13d09782eabb494fa9483ae29a4
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-15 16:23:47 +0200

    refine styles

--- src/components/Hero/Hobbies/index.tsx
+++ src/components/Hero/Hobbies/index.tsx
@@ -12,9 +12,7 @@ const InterestsHero = ({ imgSrc, children, animationDelay, animationDuration }: 
     className="flex flex-col px-6 py-4 overflow-hidden transition-colors duration-200 ease-in-out rounded-lg md:flex-row-reverse md:gap-8 xl:col-span-2 bg-neutral-100 hover:bg-neutral-200 lg:justify-between md:px-8 md:py-6 xl:max-w-5xl xl:m-auto xl:h-full"
   >
     <div className="flex flex-col justify-start flex-1 order-last mt-4 lg:m-0">
-      <div>
-        {children}
-      </div>
+      {children}
     </div>
     {imgSrc &&
       <div className="relative flex justify-center order-first h-60 md:h-full md:w-[20%] xl:w-[40%]">
--- src/components/Hobbies/Item/index.tsx
+++ src/components/Hobbies/Item/index.tsx
@@ -1,6 +1,5 @@
-import { v4 as uuidv4 } from 'uuid';
-import InterestsHero from '../../Hero/Hobbies';
-import { H2, P } from '../../shared/Text';
+import InterestsHero from 'src/components/Hero/Hobbies';
+import { H2, H3, P } from '../../shared/Text';
 
 export interface InterestItem {
   title: string
@@ -15,13 +14,13 @@ interface ItemProps {
 
 const Item = ({ item: { title, paragraphs, imagePath }, animationDelay }: ItemProps) => (
   <InterestsHero imgSrc={imagePath} animationDelay={animationDelay}>
-    <div>
-      <h2 className="box-border font-serif text-xl leading-tight tracking-tight text-center font-regular md:text-2xl lg:text-3xl text-primary-800 md:text-left">
-        {title}
-      </h2>
-      <div>
-        {paragraphs.map((paragraph: string, i: number) => <P key={`hobbies-item-p-${i}`} className="mt-4 md:mt-6">{paragraph}</P>)}
-      </div>
+    <H3 className="text-center text-primary-800 md:text-left">{title}</H3>
+    <div className='mt-1'>
+      {paragraphs.map((paragraph: string, i: number) => (
+        <P key={`hobbies-item-p-${i}`} className="mt-2 md:mt-4">
+          {paragraph}
+        </P>
+      ))}
     </div>
   </InterestsHero>
 );

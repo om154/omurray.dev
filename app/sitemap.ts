7de4d211df8dc5543a9aee9b0988d28d49bd1f8c
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-24 15:57:41 +0200

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://omurray.dev',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://omurray.dev/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://omurray.dev/hobbies',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://omurray.dev/travel',
+      lastModified: new Date(),
+    },
+  ]
+}

d95e23ea792115a75938dfe6acea81c49520aca0
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-07-24 16:03:18 +0200

    add a custom 404 - Not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import { H1 } from 'src/components/shared/Text'
+
+export default function NotFound() {
+  return (
+    <div>
+      <H1>Oops! Page not found.</H1>
+      <H1 className='mt-2'>
+        <Link
+          href='/'
+          className='inline-block text-primary-600 decoration-[transparent] hover:decoration-primary-700 hover:text-primary-700 transition-colors duration-300 ease-in-out underline-offset-[6px] break-inside-avoid'
+        >
+          Go to home ⤴️
+        </Link>
+      </H1>
+    </div>
+  )
+}

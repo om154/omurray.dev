ebb877309c434b87d8d87c53b5159707ff1bc068
Oliver Murray <20854536+om154@users.noreply.github.com>
2023-08-15 18:59:26 -0700

    refine copy and styling for work experience

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -28,25 +28,34 @@ export function calculateAge(birthday: string) {
   return age;
 }
 
-interface AboutMeCardProps {
+interface AboutMeCardProps extends AboutMeCard {
   index: number
-  content: ReactNode
-  Pill: ReactNode
 }
 
-const AboutMeCard = ({ index, content, Pill }: AboutMeCardProps) => (
-  <AnimateDown delay={.1 + (index * .1)} className='flex flex-col justify-between p-5 font-sans transition-colors duration-200 ease-in-out rounded-lg aspect-squar bg-neutral-100 hover:bg-neutral-200'>
-    <P>
-      {content}
-    </P>
-    <div className="flex flex-col items-center mt-3 space-x-2 lg:space-x-0 lg:flex-row lg:justify-around">
-      {Pill}
+const AboutMeCard = ({ index, content, stage, year, Pill }: AboutMeCardProps) => (
+  <AnimateDown delay={.1 + (index * .1)} className='flex flex-col justify-between px-5 pt-3 pb-5 font-sans transition-colors duration-200 ease-in-out rounded-lg aspect-squar bg-neutral-100 hover:bg-neutral-200'>
+    <div>
+      <div className='mb-2 text-sm text-left text-neutral-600'>{year}</div>
+      <P>
+        {content}
+      </P>
+    </div>
+    <div>
+      {/* <div className='flex flex-row justify-between'> */}
+
+        {stage ? <div className='mt-2 text-sm text-right text-neutral-600'>{stage}</div> : <div />}
+      {/* </div> */}
+      <div className="flex flex-col items-center mt-3 space-x-2 lg:space-x-0 lg:flex-row lg:justify-around">
+        {Pill}
+      </div>
     </div>
   </AnimateDown>
 )
 
 interface AboutMeCard {
   content: ReactNode
+  year: string
+  stage?: string
   Pill: ReactNode
 }
 
@@ -55,49 +64,53 @@ const cards: AboutMeCard[] = [
   {
     content: <>
       At <Link className="decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.twine.us/" target="_blank" rel="noreferrer">twine</Link>
-      {' '}we build products to connect remote teams (Seed stage, Zoom Ventures). Our platform has powered thousands of conversations!
+      {' '}we build products to connect remote teams. Our platform has powered thousands of conversations!
       <div className='mt-1' />
       Our latest product -{' '}
       <Link className="decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.twine.us/ambient" target="_blank" rel="noreferrer">twine Ambient</Link>
-      {' '}- is an AI Chief of Staff that summarizes, threads, and shares important business updates from meetings, Slack, and other tools across a business.
+      {' '}- is an AI Chief of Staff that summarizes, threads, and shares important context from meetings, Slack, and other sources from across your business.
     </>,
+    year: '2022 - 2023',
+    stage: 'Seed stage, Zoom Ventures',
     Pill: <TwinePill className="my-1 select-none lg:m-0" />
   },
   // Glimpse
   {
     content: <>
-      I began working to connect remote teams as Founding Engineer at
+      At{' '}
+      <Link className="inline whitespace-break-spaces decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.joinglimpse.com/" target="_blank" rel="noreferrer">Glimpse</Link>
+      {' '}I joined 4 ex-Duke students as was the first external hire. I worked with the founders to define the platform architecture and shipped new features to scale and refine our video chat platform.
       {' '}
-      <Link className="inline whitespace-break-spaces decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.joinglimpse.com/" target="_blank" rel="noreferrer">Glimpse (Seed stage, Y Combinator)</Link>
-      .
-      <div className='mt-1' />
-      As the first external hire, I helped define the application's architecture and build the first version of the product.
-      <div className='mt-1' />
-      We teamed up with twine through an acquisition in early 2022, and I took on role leading development of our newly launched Zoom product.
+      <div className='mt-1 '/>
+      I also lead the development of our Zoom app, which ultimately made us a target for acquisition.
     </>,
+    year: '2021 - 2022',
+    stage: 'Seed stage, Y Combinator',
     Pill: <GlimpsePill className="mt-1 select-none lg:m-0" />
   },
   // Shippit
   {
     content: <>
-      I worked in Engineering at a Sydney-based start-up named
+      I previously worked in Engineering at a Sydney-based start-up named
       {' '}
-      <Link className="decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.shippit.com/" target="_blank" rel="noreferrer">Shippit (Series B, Tiger Global)</Link>.
-      <div className='mt-1' />
-      They have a SaaS product for ecommerce merchants and are working to bring people & goods together in the leanest way possible.
+      <Link className="decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.shippit.com/" target="_blank" rel="noreferrer">Shippit</Link>.
+      {' '}Their SaaS product helps streamline shipping & fulfilment for retailers.
     </>,
+    year: '2020 - 2021',
+    stage: 'Series B, Tiger Global',
     Pill: <ShippitPill className="my-1 select-none lg:m-0" />
   },
   // Accenture
   {
     content: <>
-      I spent 3 years at
+      At
       {' '}
       <Link className="decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.accenture.com/" target="_blank" rel="noreferrer">Accenture</Link>
       {' '}
-      developing large-scale enterprise
-      applications to help their clients effectively capture, manage and analyze data from dozens of sources.
+      I worked on multiple projects developing large-scale enterprise
+      applications to help clients effectively capture, manage and analyze data from dozens of sources.
     </>,
+    year: '2018 - 2020',
     Pill: <AccenturePill className="my-1 select-none lg:my-0 lg:ml-1" />
   }
 ]
@@ -112,7 +125,7 @@ const AboutMe = () => (
         <P>
           Hey 👋🏻 I&apos;m Oliver, a
           {process.env.NEXT_PUBLIC_DATE_OF_BIRTH && ` ${calculateAge(process.env.NEXT_PUBLIC_DATE_OF_BIRTH)} year old `}
-          Software Engineer originally from Melbourne, Australia. I moved to the United States 🇺🇸 in 2021 and I'm now working on connecting remote teams at
+          Software Engineer originally from Melbourne, Australia. I moved to the United States 🇺🇸 in 2021 and I'm now working on the problem of connection & context for remote teams at
           {' '}
           <Link className="decoration-[transparent] hover:decoration-primary-900 hover:text-primary-900" href="https://www.twine.us/" target="_blank" rel="noreferrer">twine</Link>
           !
@@ -126,7 +139,7 @@ const AboutMe = () => (
         </div>
         <div className='grid grid-cols-1 gap-4 mt-4 sm:grid-flow-row-dense lg:grid-cols-2'>
           {cards.map((c, i) => (
-            <AboutMeCard index={i} content={c.content} Pill={c.Pill} />
+            <AboutMeCard index={i} content={c.content} Pill={c.Pill} stage={c.stage} year={c.year} />
           ))}
         </div>
       </AboutMeHero>

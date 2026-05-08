d3a3493d3fca79562a7fe7351a3deedf210439de
Oliver Murray <20854536+om154@users.noreply.github.com>
2026-05-08 16:29:14 -0600

    add Trail Challenge

--- app/projects/ProjectItem.tsx
+++ app/projects/ProjectItem.tsx
@@ -1,22 +1,25 @@
-import Image from "next/image"
-import Link from "next/link"
-import AnimateDown from "src/components/animations/AnimateDown"
-import AnimateFadeIn from "src/components/animations/AnimateFadeIn"
-import { P } from "src/components/shared/Text"
+import AnimateDown from "src/components/animations/AnimateDown";
+import AnimateFadeIn from "src/components/animations/AnimateFadeIn";
+import Image from "next/image";
+import Link from "next/link";
+import { P } from "src/components/shared/Text";
 
 export interface ProjectItemModel {
-  url: string
-  paragraphs: string[]
-  technologies: string[]
-  imagePath: string
+  url: string;
+  paragraphs: string[];
+  technologies: string[];
+  imagePath: string;
 }
 
 interface ProjectItemProps {
-  item: ProjectItemModel
-  animationDelay: number
+  item: ProjectItemModel;
+  animationDelay: number;
 }
 
-const ProjectItem = ({ item: { paragraphs, technologies, url, imagePath }, animationDelay }: ProjectItemProps) => (
+const ProjectItem = ({
+  item: { paragraphs, technologies, url, imagePath },
+  animationDelay,
+}: ProjectItemProps) => (
   <AnimateDown
     delay={animationDelay}
     className="flex flex-col w-full px-6 py-4 overflow-hidden transition-colors duration-200 ease-in-out rounded-lg md:flex-row-reverse md:gap-8 xl:col-span-2 bg-neutral-100 hover:bg-neutral-200 lg:justify-between md:px-8 md:py-6 xl:max-w-5xl xl:m-auto xl:h-full"
@@ -24,13 +27,20 @@ const ProjectItem = ({ item: { paragraphs, technologies, url, imagePath }, anima
     <div className="flex flex-col justify-between flex-1 order-last mt-4 md:m-0">
       <div>
         <div className="flex flex-row justify-start">
-          <Link href={url} className="overflow-hidden text-center text-ellipsis text-primary-700 md:text-left hover:text-primary-800" target="_blank" rel="noreferrer" style={{ fontWeight:700}}>
+          <Link
+            href={url}
+            className="overflow-hidden text-center text-ellipsis text-primary-700 md:text-left hover:text-primary-800 font-extrabold"
+            target="_blank"
+            rel="noreferrer"
+          >
             <AnimateFadeIn duration={0.35}>
-              <h3 className='overflow-hidden text-ellipsis box-border font-mono text-xl leading-tight lg:text-2xl underline decoration-primary-700 decoration-1 hover:decoration-primary-800 transition-[color,text-decoration] duration-300 ease'>{url}</h3>
+              <h3 className="overflow-hidden text-ellipsis box-border font-mono text-lg leading-tight lg:text-xl underline decoration-primary-700 decoration-1 hover:decoration-primary-800 transition-[color,text-decoration] duration-300 ease">
+                {url}
+              </h3>
             </AnimateFadeIn>
           </Link>
         </div>
-        <div className='mt-4'>
+        <div className="mt-4">
           {paragraphs.map((paragraph: string, i: number) => (
             <P key={`hobbies-item-p-${i}`} className="mt-1">
               {paragraph}
@@ -39,16 +49,27 @@ const ProjectItem = ({ item: { paragraphs, technologies, url, imagePath }, anima
         </div>
       </div>
       <div>
-        <div className='w-full my-3 border-[0.5px] border-black border-opacity-20 border-solid' />
-        <div className="self-end">
-          <span className=" text-black/40">Built with </span>{technologies.map((t, i) => <span className="text-black/40">{i === technologies.length - 1 ? ' and ' : ' '}{t}{i === technologies.length - 1 ? '' : ','}</span>)}
+        <div className="w-full my-3 border-[0.5px] border-black border-opacity-20 border-solid" />
+        <div className="self-end text-black/40">
+          {technologies.map((t, i) => (
+            <span key={`tech-${i}`}>
+              {i > 0 && <span>・</span>}
+              {t}
+            </span>
+          ))}
         </div>
       </div>
     </div>
-    <div className="relative flex justify-center order-first h-60 md:h-full md:w-[20%] xl:w-[40%] shadow-md rounded-md">
-      <Image src={imagePath} fill placeholder='blur' alt='interest' className="object-cover object-top max-w-full rounded-md select-none" />
+    <div className="relative flex justify-center order-first h-60 md:h-full md:w-[20%] xl:w-[40%] md:shrink-0 shadow-md rounded-md">
+      <Image
+        src={imagePath}
+        fill
+        placeholder="blur"
+        alt="interest"
+        className="object-cover object-top max-w-full rounded-md select-none"
+      />
     </div>
   </AnimateDown>
 );
 
-export default ProjectItem
+export default ProjectItem;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,115 +1,130 @@
-import Section from 'src/components/Section';
-import Item, { InterestItem } from 'src/components/Hobbies/Item';
-import { H2, P } from 'src/components/shared/Text';
-import comedyFyiImg from 'public/static/img/comedyFyi.png'
-import firesideImg from 'public/static/img/fireside.png'
-import backyardImg from 'public/static/img/backyard.png'
-import thinValueImg from 'public/static/img/thinvaluePoker.png'
-import omurrayDevImg from 'public/static/img/omurrayDev.png'
-import { Metadata } from 'next'
-import ProjectItem, { ProjectItemModel } from './ProjectItem';
+import { H2, P } from "src/components/shared/Text";
+import Item, { InterestItem } from "src/components/Hobbies/Item";
+import ProjectItem, { ProjectItemModel } from "./ProjectItem";
+
+import { Metadata } from "next";
+import Section from "src/components/Section";
+import backyardImg from "public/static/img/backyard.png";
+import comedyFyiImg from "public/static/img/comedyFyi.png";
+import firesideImg from "public/static/img/fireside.png";
+import omurrayDevImg from "public/static/img/omurrayDev.png";
+import thinValueImg from "public/static/img/thinvaluePoker.png";
+import trailchallengeImg from "public/static/img/trailchallenge.png";
 
 export const metadata: Metadata = {
-  title: 'Projects'
-}
+  title: "Projects",
+};
 
 const items: ProjectItemModel[] = [
   {
     paragraphs: [
-      'An app to find tickets to shows by the United States\' best comedians.',
-      'For any show, you can find a link to buy tickets and see other shows nearby on a map.',
+      "Discover and complete all of your city's best trails.",
+      "Find every trail in your town, track your progress in real time, and earn rewards once you've completed them all.",
     ],
     technologies: [
-      'Next.js',
-      'React',
-      'TypeScript',
-      'Tailwind',
-      'Mapbox',
-      'Vercel'
+      "Astro",
+      "TypeScript",
+      "Tailwind",
+      "Cloudflare",
+      "Expo",
+      "React Native",
+      "Tamagui",
     ],
-    url: 'https://comedy.fyi',
-    imagePath: comedyFyiImg
+    url: "https://trailchallenge.co",
+    imagePath: trailchallengeImg,
   },
   {
     paragraphs: [
-      'Backyard is a platform that allows anyone to rent space, or make money renting out their space for storage. Launching soon!',
+      "A platform to find tickets to shows by the United States' best comedians.",
+      "For any show, you can find a link to buy tickets and see other shows nearby on a map.",
     ],
     technologies: [
-      'React',
-      'Remix',
-      'Vite',
-      'TypeScript',
-      'Tailwind',
-      'Supabase',
-      'Postgres',
-      'Severless on AWS (sst)',
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind",
+      "Mapbox",
+      "Vercel",
     ],
-    url: 'https://usebackyard.com',
-    imagePath: backyardImg
+    url: "https://comedy.fyi",
+    imagePath: comedyFyiImg,
   },
   {
     paragraphs: [
-      'Fireside helps you record a podcast with your loved ones. We generate nostalgic, sentimental questions for you and faciliate recording and storage so you can focus on their stories.',
-      'Launching soon!'
+      "Backyard is a platform that allows anyone to rent space, or make money renting out their space for storage.",
     ],
     technologies: [
-      'React',
-      'Remix',
-      'TypeScript',
-      'Tailwind',
-      'Neon',
-      'Severless on AWS (sst)',
+      "React",
+      "Remix",
+      "Vite",
+      "TypeScript",
+      "Tailwind",
+      "Supabase",
+      "Postgres",
+      "Severless on AWS (sst)",
     ],
-    url: 'https://recordfireside.com',
-    imagePath: firesideImg
+    url: "https://usebackyard.com",
+    imagePath: backyardImg,
   },
   {
     paragraphs: [
-      'A personal website for my brother, Lachlan, who is an amatuer poker player working to get staked as a professional.',
+      "Fireside helps you record a podcast with your loved ones. We generate nostalgic, sentimental questions for you and faciliate recording and storage so you can focus on their stories.",
     ],
     technologies: [
-      'Next.js',
-      'React',
-      'TypeScript',
-      'Tailwind',
-      'Vercel'
+      "React",
+      "Remix",
+      "TypeScript",
+      "Tailwind",
+      "Neon",
+      "Severless on AWS (sst)",
     ],
-    url: 'https://thinvalue.poker',
-    imagePath: thinValueImg
+    url: "https://recordfireside.com",
+    imagePath: firesideImg,
   },
   {
     paragraphs: [
-      'You\'re here! 🎉',
-      'A personal website about me including my travels, projects and hobbies.',
+      "A personal website for my brother, Lachlan, who is an amatuer poker player working to get staked as a professional.",
+    ],
+    technologies: ["Next.js", "React", "TypeScript", "Tailwind", "Vercel"],
+    url: "https://thinvalue.poker",
+    imagePath: thinValueImg,
+  },
+  {
+    paragraphs: [
+      "You're here! 🎉",
+      "A personal website about me including my travels, projects and hobbies.",
     ],
     technologies: [
-      'Next.js',
-      'React',
-      'TypeScript',
-      'Tailwind',
-      'Framer',
-      'Vercel'
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind",
+      "Framer",
+      "Vercel",
     ],
-    url: 'https://omurray.dev',
-    imagePath: omurrayDevImg
+    url: "https://omurray.dev",
+    imagePath: omurrayDevImg,
   },
 ];
 
-
 export default function Page() {
-  return <Section alignTop>
-    <div className="w-full text-left">
-      <H2>Projects</H2>
-      <P className='mt-2 text-neutral-900/90'>A few things I've been working on lately</P>
-    </div>
-    <div className='box-border grid grid-cols-1 gap-4 mt-4 md:grid-flow-row-dense md:auto-rows-fr md:grid-cols-1 xl:grid-cols-4'>
-      {items.map((item: ProjectItemModel, index: number) => (
-        <ProjectItem
-          key={`hobbies-grid-item-${index}`}
-          item={item}
-          animationDelay={(index + 1) * .15}
-        />
-      ))}
-    </div>
-  </Section>
+  return (
+    <Section alignTop>
+      <div className="w-full text-left">
+        <H2>Projects</H2>
+        <P className="mt-2 text-neutral-900/90">
+          A few things I've been working on lately
+        </P>
+      </div>
+      <div className="box-border grid grid-cols-1 gap-4 mt-4 md:grid-flow-row-dense md:auto-rows-fr md:grid-cols-1 xl:grid-cols-4">
+        {items.map((item: ProjectItemModel, index: number) => (
+          <ProjectItem
+            key={`hobbies-grid-item-${index}`}
+            item={item}
+            animationDelay={(index + 1) * 0.15}
+          />
+        ))}
+      </div>
+    </Section>
+  );
 }
